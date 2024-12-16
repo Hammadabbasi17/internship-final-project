088c4d6f6fcde94707d48cdfc9fbede342a85669
Hammad <hammadabbasi1700@gmail.com>
2024-12-16 07:03:02 -0800

    update

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <p className='text-center text-[30px]'>hello hammad</p>
       <div className='w-full h-auto flex flex-col lg:flex-row justify-evenly items-center gap-9 mt-[120px]'>
         <div className='w-auto lg:w-[340px] h-auto '>
           <div className='w-[262px] h-[122px] '>
